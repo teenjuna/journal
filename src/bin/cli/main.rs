fc9b1d1dfbc423be750f9c327b214a81d90ad920
Alex Ryapolov <ryapolov@pm.me>
2020-12-11 15:33:41 +0700

    Save data in appropriate directory

--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -1,7 +1,9 @@
 mod write;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Clap;
+use directories::BaseDirs;
+use std::fs;
 use std::path::Path;
 
 #[derive(Clap)]
@@ -16,9 +18,17 @@ enum SubCommand {
 }
 
 fn main() -> Result<()> {
+    // Setup directory for app data.
+    let entries_file = {
+        let base_dirs = BaseDirs::new().ok_or_else(|| anyhow!("base dirs are not found"))?;
+        let data_dir = &base_dirs.data_dir().join("journal");
+        fs::create_dir_all(data_dir)?;
+        data_dir.join("entries.json")
+    };
+
     // Create journal instance.
     let journal = {
-        let entry_storage = journal::json::JsonEntryStorage::new(Path::new("/tmp/entries.json"))?;
+        let entry_storage = journal::json::JsonEntryStorage::new(Path::new(&entries_file))?;
         journal::Journal::new(entry_storage)
     };
 
