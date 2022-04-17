34b07568c69335eb173780a26261ddf4a9fb3bb4
Alex Ryapolov <ryapolov@pm.me>
2022-04-17 16:32:50 +0700

    Make `list` cmd list entries from older to newer

--- src/bin/cli/list.rs
+++ src/bin/cli/list.rs
@@ -1,21 +1,30 @@
 use anyhow::Result;
 use clap::Args;
-use journal::entry::EntryStorage;
+use journal::entry::{Entry, EntryStorage};
 use journal::Journal;
 
 /// Prints IDs of all saved entries
 #[derive(Args)]
-pub struct Opts {}
+pub struct Opts {
+    /// List entries in reverse order.
+    #[clap(short, long)]
+    reverse: bool,
+}
 
-pub fn execute<E: EntryStorage>(
-    journal: Journal<E>,
-    _opts: Opts,
-) -> Result<()> {
+pub fn execute<E: EntryStorage>(journal: Journal<E>, opts: Opts) -> Result<()> {
     let entries = match &journal {
         Journal::Plain(j) => j.get_entries()?,
     };
 
-    entries.iter().for_each(|e| println!("{}", e.id));
+    let entries: Box<dyn Iterator<Item = &Entry>> = if opts.reverse {
+        // From newer to older.
+        Box::new(entries.iter())
+    } else {
+        // From older to newer by default.
+        Box::new(entries.iter().rev())
+    };
+
+    entries.for_each(|e| println!("{}", e.id));
 
     Ok(())
 }
